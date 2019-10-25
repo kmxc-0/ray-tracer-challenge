6e922aba01b91c0c0a8d2e702b74f6be5e8bf998
kmxc-0 <nyugenchan3@gmail.com>
2019-10-25 14:20:22 -0500

    Further transformations
    -Rotation around x, y and z axis.
    -Shearing in proportion for each axis.

--- ray-tracer/src/app/transformations/transformation.service.spec.ts
+++ ray-tracer/src/app/transformations/transformation.service.spec.ts
@@ -3,6 +3,7 @@ import { TransformationService } from "./transformation.service";
 import { Point } from "../models/tuples/point";
 import { Matrix } from "../models/matrices/matrix";
 import { Vector } from "../models/tuples/vector";
+import { Tuple } from "../models/tuples/tuple";
 
 describe("TransformationService", () => {
   let service: TransformationService;
@@ -57,6 +58,140 @@ describe("TransformationService", () => {
       const point = new Point(2, 3, 4);
 
       expect(transform.multiply(point)).toEqual(new Point(-2, 3, 4));
+    });
+  });
+
+  describe("rotationX", () => {
+    it("should rotate a half quarter", () => {
+      const halfQuarter = Math.PI / 4;
+      const transform: Matrix = service.rotationX(halfQuarter);
+      const point = new Point(0, 1, 0);
+
+      const result = transform.multiply(point) as Tuple;
+      expect(result.x).toEqual(0);
+      expect(result.y).toBeCloseTo(Math.SQRT2 / 2, 5);
+      expect(result.z).toBeCloseTo(Math.SQRT2 / 2, 5);
+      expect(result.w).toEqual(1);
+    });
+
+    it("should rotate a full quarter", () => {
+      const fullQuarter = Math.PI / 2;
+      const transform: Matrix = service.rotationX(fullQuarter);
+      const point = new Point(0, 1, 0);
+
+      expect(transform.multiply(point)).toEqual(new Point(0, 0, 1));
+    });
+  });
+
+  describe("rotationY", () => {
+    it("should rotate a half quarter", () => {
+      const halfQuarter = Math.PI / 4;
+      const transform: Matrix = service.rotationY(halfQuarter);
+      const point = new Point(0, 0, 1);
+
+      const result = transform.multiply(point) as Tuple;
+      expect(result.x).toBeCloseTo(Math.SQRT2 / 2, 5);
+      expect(result.y).toEqual(0);
+      expect(result.z).toBeCloseTo(Math.SQRT2 / 2, 5);
+      expect(result.w).toEqual(1);
+    });
+
+    it("should rotate a full quarter", () => {
+      const fullQuarter = Math.PI / 2;
+      const transform: Matrix = service.rotationY(fullQuarter);
+      const point = new Point(0, 0, 1);
+
+      expect(transform.multiply(point)).toEqual(new Point(1, 0, 0));
+    });
+  });
+
+  describe("rotationZ", () => {
+    it("should rotate a half quarter", () => {
+      const halfQuarter = Math.PI / 4;
+      const transform: Matrix = service.rotationZ(halfQuarter);
+      const point = new Point(0, 1, 0);
+
+      const result = transform.multiply(point) as Tuple;
+      expect(result.x).toBeCloseTo(-Math.SQRT2 / 2, 5);
+      expect(result.y).toBeCloseTo(Math.SQRT2 / 2, 5);
+      expect(result.z).toEqual(0);
+      expect(result.w).toEqual(1);
+    });
+
+    it("should rotate a full quarter", () => {
+      const fullQuarter = Math.PI / 2;
+      const transform: Matrix = service.rotationZ(fullQuarter);
+      const point = new Point(0, 1, 0);
+
+      expect(transform.multiply(point)).toEqual(new Point(-1, 0, 0));
+    });
+  });
+
+  describe("shearing", () => {
+    it("should apply a shearing transformation where x moves in proportion to y", () => {
+      const transform: Matrix = service.shearing(1, 0, 0, 0, 0, 0);
+      const point = new Point(2, 3, 4);
+      expect(transform.multiply(point)).toEqual(new Point(5, 3, 4));
+    });
+
+    it("should apply a shearing transformation where x moves in proportion to z", () => {
+      const transform: Matrix = service.shearing(0, 1, 0, 0, 0, 0);
+      const point = new Point(2, 3, 4);
+      expect(transform.multiply(point)).toEqual(new Point(6, 3, 4));
+    });
+
+    it("should apply a shearing transformation where y moves in proportion to x", () => {
+      const transform: Matrix = service.shearing(0, 0, 1, 0, 0, 0);
+      const point = new Point(2, 3, 4);
+      expect(transform.multiply(point)).toEqual(new Point(2, 5, 4));
+    });
+
+    it("should apply a shearing transformation where y moves in proportion to z", () => {
+      const transform: Matrix = service.shearing(0, 0, 0, 1, 0, 0);
+      const point = new Point(2, 3, 4);
+      expect(transform.multiply(point)).toEqual(new Point(2, 7, 4));
+    });
+
+    it("should apply a shearing transformation where y moves in proportion to z", () => {
+      const transform: Matrix = service.shearing(0, 0, 0, 0, 1, 0);
+      const point = new Point(2, 3, 4);
+      expect(transform.multiply(point)).toEqual(new Point(2, 3, 6));
+    });
+
+    it("should apply a shearing transformation where z moves in proportion to y", () => {
+      const transform: Matrix = service.shearing(0, 0, 0, 0, 0, 1);
+      const point = new Point(2, 3, 4);
+      expect(transform.multiply(point)).toEqual(new Point(2, 3, 7));
+    });
+  });
+
+  describe("transformation chaining", () => {
+    it("should apply transformations in sequence", () => {
+      const point = new Point(1, 0, 1);
+      const rotate = service.rotationX(Math.PI / 2);
+      const scaling = service.scaling(5, 5, 0);
+      const translation = service.translation(10, 5, 7);
+
+      const rotatedP = rotate.multiply(point);
+      expect(rotatedP).toEqual(new Point(1, -1, 0));
+      const scalingP = scaling.multiply(rotatedP);
+      expect(scalingP).toEqual(new Point(5, -5, 0));
+      const translatedP = translation.multiply(scalingP);
+
+      expect(translatedP).toEqual(new Point(15, 0, 7));
+    });
+
+    it("should apply transformations in reversed order", () => {
+      const point = new Point(1, 0, 1);
+      const rotate = service.rotationX(Math.PI / 2);
+      const scaling = service.scaling(5, 5, 0);
+      const translation = service.translation(10, 5, 7);
+
+      const transformation = (translation.multiply(scaling) as Matrix).multiply(
+        rotate
+      ) as Matrix;
+
+      expect(transformation.multiply(point)).toEqual(new Point(15, 0, 7));
     });
   });
 });
--- ray-tracer/src/app/transformations/transformation.service.ts
+++ ray-tracer/src/app/transformations/transformation.service.ts
@@ -18,4 +18,48 @@ export class TransformationService {
     const wr = [0, 0, 0, 1];
     return new Matrix(xr, yr, zr, wr);
   }
+
+  rotationX(r: number) {
+    const fixed = (num: number): number => +num.toFixed(5);
+    const fixedR = fixed(r);
+    const xr = [1, 0, 0, 0];
+    const yr = [0, fixed(Math.cos(fixedR)), fixed(-Math.sin(fixedR)), 0];
+    const zr = [0, fixed(Math.sin(fixedR)), fixed(Math.cos(fixedR)), 0];
+    const wr = [0, 0, 0, 1];
+
+    return new Matrix(xr, yr, zr, wr);
+  }
+
+  rotationY(r: number) {
+    const fixed = (num: number): number => +num.toFixed(5);
+    const fixedR = fixed(r);
+
+    const xr = [fixed(Math.cos(fixedR)), 0, fixed(Math.sin(fixedR)), 0];
+    const yr = [0, 1, 0, 0];
+    const zr = [fixed(-Math.sin(fixedR)), 0, fixed(Math.cos(fixedR)), 0];
+    const wr = [0, 0, 0, 1];
+
+    return new Matrix(xr, yr, zr, wr);
+  }
+
+  rotationZ(r: number) {
+    const fixed = (num: number): number => +num.toFixed(5);
+    const fixedR = fixed(r);
+
+    const xr = [fixed(Math.cos(fixedR)), fixed(-Math.sin(fixedR)), 0, 0];
+    const yr = [fixed(Math.sin(fixedR)), fixed(Math.cos(fixedR)), 0, 0];
+    const zr = [0, 0, 1, 0];
+    const wr = [0, 0, 0, 1];
+
+    return new Matrix(xr, yr, zr, wr);
+  }
+
+  shearing(xy, xz, yx, yz, zx, zy) {
+    const xs = [1, xy, xz, 0];
+    const ys = [yx, 1, yz, 0];
+    const zs = [zx, zy, 1, 0];
+    const ws = [0, 0, 0, 1];
+
+    return new Matrix(xs, ys, zs, ws);
+  }
 }
